7a18db29721a06710acdf74d01d0899b5ee6625a
StudioWEngineers <studio.w.engineers@gmail.com>
2025-04-03 12:49:39 +0200

    even more aggressive clean-up

--- src/librhino3dm_native/curvedisplay.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-//class CCurveDisplay
-//{
-//  ON_ClassArray<ON_BezierCurve> m_bezs;
-//public:
-//  void Update(const ON_NurbsCurve& nurbs_curve);
-//  void Draw(CRhinoDisplayPipeline& dp);
-//};

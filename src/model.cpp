b5e5e76c25dbe322020c86b36c2f4a09b57a18d5
StudioWEngineers <studio.w.engineers@gmail.com>
2025-04-28 17:12:47 +0200

    more clean-up model

--- src/model.cpp
+++ src/model.cpp
@@ -1,114 +1,22 @@
 #include "bindings.h"
 #include "base64.h"
 
-// TODO: Move some of this functionality into core opennurbs
-static bool SeekPastCompressedBuffer(ON_BinaryArchive& archive)
-{
-  if (!archive.ReadMode())
-    return false;
-
-  bool rc = false;
-  char method = 0;
-  unsigned int buffer_crc0 = 0;
-
-  size_t sizeof__outbuffer;
-  if (!archive.ReadCompressedBufferSize(&sizeof__outbuffer))
-    return false;
-
-  if (0 == sizeof__outbuffer)
-    return true;
-
-  if (!archive.ReadInt(&buffer_crc0)) // 32 bit crc of uncompressed buffer
-    return false;
-
-  if (!archive.ReadChar(&method))
-    return false;
-
-  if (method != 0 && method != 1)
-    return false;
-
-  switch (method)
-  {
-  case 0: // uncompressed
-    rc = archive.SeekForward(sizeof__outbuffer);
-    break;
-  case 1: // compressed
-    {
-    ON__UINT32 tcode = 0;
-    ON__INT64  big_value = 0;
-    rc = archive.BeginRead3dmBigChunk(&tcode, &big_value);
-    if (rc)
-      rc = archive.EndRead3dmChunk();
-    }
-    break;
-  }
-
-  return rc;
-}
-
-
-BND_TUPLE BND_ONXModel::GetEmbeddedFilePaths()
-{
-  ON_ClassArray<ON_wString> paths;
-  ON_SimpleArray<ONX_Model_UserData*>& userdata_table = m_model->m_userdata_table;
-  for (int i = 0; i < userdata_table.Count(); i++)
-  {
-    ONX_Model_UserData* ud = userdata_table[i];
-    if (ud && m_model->GetRDKEmbeddedFilePaths(*ud, paths))
-    //if (ud && GetRDKEmbeddedFileHelper(*ud, paths, nullptr, nullptr, false))
-      break;
-  }
-  int count = paths.Count();
-
-  BND_TUPLE rc = CreateTuple(count);
-  for (int i = 0; i < count; i++)
-  {
-    std::wstring path(paths[i].Array());
-    SetTuple(rc, i, path);
-  }
-  return rc;
-}
-
-std::vector<std::wstring> BND_ONXModel::GetEmbeddedFilePaths2()
-{
-  ON_ClassArray<ON_wString> paths;
-  ON_SimpleArray<ONX_Model_UserData*>& userdata_table = m_model->m_userdata_table;
-  for (int i = 0; i < userdata_table.Count(); i++)
-  {
-    ONX_Model_UserData* ud = userdata_table[i];
-    if (ud && m_model->GetRDKEmbeddedFilePaths(*ud, paths))
-    //if (ud && GetRDKEmbeddedFileHelper(*ud, paths, nullptr, nullptr, false))
-      break;
-  }
-  int count = paths.Count();
-
-  std::vector<std::wstring> rc;
-  for (int i = 0; i < count; i++)
-  {
-    std::wstring path(paths[i].Array());
-    rc.push_back(path);
-  }
-  return rc;
-}
-
-///////////////////////////////////////////////
-
 BND_ONXModel::BND_ONXModel()
 {
-  m_model.reset(new ONX_Model());
+  p_model.reset(new ONX_Model());
 }
 
 BND_ONXModel::BND_ONXModel(ONX_Model* m)
 {
-  m_model.reset(m);
+  p_model.reset(m);
 }
 
 void BND_ONXModel::Destroy() {
-    ONX_Model* model = m_model.get();
+    ONX_Model* model = p_model.get();
     if(model) {
         delete model;
     }
-    m_model.reset(nullptr);
+    p_model.reset(nullptr);
 }
 
 BND_ONXModel* BND_ONXModel::Read(std::wstring path)
@@ -167,18 +75,18 @@ int BND_ONXModel::ReadArchiveVersion(std::wstring path)
 }
 
 bool BND_ONXModel::Write(std::wstring path, int version) {
-    return m_model->Write(path.c_str(), version);
+    return p_model->Write(path.c_str(), version);
 }
 
 std::wstring BND_ONXModel::GetStartSectionComments() const
 {
-  ON_wString comments = m_model->m_sStartSectionComments;
+  ON_wString comments = p_model->m_sStartSectionComments;
   return std::wstring(comments);
 }
 void BND_ONXModel::SetStartSectionComments(std::wstring comments)
 {
   ON_wString wcomments = comments.c_str();
-  m_model->m_sStartSectionComments = wcomments;
+  p_model->m_sStartSectionComments = wcomments;
 }
 
 const int idxApplicationName = 0;
@@ -242,56 +150,56 @@ RH_C_FUNCTION void ONX_Model_SetString(ONX_Model* pModel, int which, const wchar
 std::wstring BND_ONXModel::GetApplicationName() const
 {
   ON_wString s;
-  ONX_Model_GetString(m_model.get(), idxApplicationName, &s);
+  ONX_Model_GetString(p_model.get(), idxApplicationName, &s);
   return std::wstring(s);
 }
 void BND_ONXModel::SetApplicationName(std::wstring comments)
 {
-  ONX_Model_SetString(m_model.get(), idxApplicationName, comments.c_str());
+  ONX_Model_SetString(p_model.get(), idxApplicationName, comments.c_str());
 }
 std::wstring BND_ONXModel::GetApplicationUrl() const
 {
   ON_wString s;
-  ONX_Model_GetString(m_model.get(), idxApplicationUrl, &s);
+  ONX_Model_GetString(p_model.get(), idxApplicationUrl, &s);
   return std::wstring(s);
 }
 void BND_ONXModel::SetApplicationUrl(std::wstring s)
 {
-  ONX_Model_SetString(m_model.get(), idxApplicationUrl, s.c_str());
+  ONX_Model_SetString(p_model.get(), idxApplicationUrl, s.c_str());
 }
 std::wstring BND_ONXModel::GetApplicationDetails() const
 {
   ON_wString s;
-  ONX_Model_GetString(m_model.get(), idxApplicationDetails, &s);
+  ONX_Model_GetString(p_model.get(), idxApplicationDetails, &s);
   return std::wstring(s);
 }
 void BND_ONXModel::SetApplicationDetails(std::wstring s)
 {
-  ONX_Model_SetString(m_model.get(), idxApplicationDetails, s.c_str());
+  ONX_Model_SetString(p_model.get(), idxApplicationDetails, s.c_str());
 }
 int BND_ONXModel::GetArchiveVersion() const
 {
-  return m_model->m_3dm_file_version;
+  return p_model->m_3dm_file_version;
 }
 std::wstring BND_ONXModel::GetCreatedBy() const
 {
   ON_wString s;
-  ONX_Model_GetString(m_model.get(), idxCreatedBy, &s);
+  ONX_Model_GetString(p_model.get(), idxCreatedBy, &s);
   return std::wstring(s);
 }
 std::wstring BND_ONXModel::GetLastEditedBy() const
 {
   ON_wString s;
-  ONX_Model_GetString(m_model.get(), idxLastCreatedBy, &s);
+  ONX_Model_GetString(p_model.get(), idxLastCreatedBy, &s);
   return std::wstring(s);
 }
 BND_DateTime BND_ONXModel::GetCreated() const
 {
-  return CreateDateTime(m_model->m_properties.m_RevisionHistory.m_create_time);
+  return CreateDateTime(p_model->m_properties.m_RevisionHistory.m_create_time);
 }
 BND_DateTime BND_ONXModel::GetLastEdited() const
 {
-  return CreateDateTime(m_model->m_properties.m_RevisionHistory.m_last_edit_time);
+  return CreateDateTime(p_model->m_properties.m_RevisionHistory.m_last_edit_time);
 }
 
 RH_C_FUNCTION int ONX_Model_GetRevision(const ONX_Model* pConstModel)
@@ -310,13 +218,15 @@ RH_C_FUNCTION void ONX_Model_SetRevision(ONX_Model* pModel, int rev)
 
 int BND_ONXModel::GetRevision() const
 {
-  return ONX_Model_GetRevision(m_model.get());
+  return ONX_Model_GetRevision(p_model.get());
 }
-void BND_ONXModel::SetRevision(int r)
-{
-  ONX_Model_SetRevision(m_model.get(), r);
+void BND_ONXModel::SetRevision(int revision_number) {
+    ONX_Model_SetRevision(p_model.get(), revision_number);
 }
 
+BND_File3dmLayerTable BND_ONXModel::Layers() {
+    return BND_File3dmLayerTable(p_model);
+}
 
 // --------------------- Iterator helpers ------- //
 template <typename IT, typename ET>
@@ -349,39 +259,7 @@ void ModelBindings(rh3dmpymodule& m)
 //      return BND_ONXModel::FromByteArray(static_cast<int>(info.size), info.ptr);
 //    })
 // #endif
-    .def("Write", &BND_ONXModel::Write, py::arg("path"), py::arg("version")=0)
-    //.def_property("StartSectionComments", &BND_ONXModel::GetStartSectionComments, &BND_ONXModel::SetStartSectionComments)
-    //.def_property("ApplicationName", &BND_ONXModel::GetApplicationName, &BND_ONXModel::SetApplicationName)
-    //.def_property("ApplicationUrl", &BND_ONXModel::GetApplicationUrl, &BND_ONXModel::SetApplicationUrl)
-    //.def_property("ApplicationDetails", &BND_ONXModel::GetApplicationDetails, &BND_ONXModel::SetApplicationDetails)
-    //.def_property_readonly("ArchiveVersion", &BND_ONXModel::GetArchiveVersion)
-    //.def_property_readonly("Created", &BND_ONXModel::GetCreated)
-    //.def_property_readonly("CreatedBy", &BND_ONXModel::GetCreatedBy)
-    //.def_property_readonly("LastEdited", &BND_ONXModel::GetLastEdited)
-    //.def_property_readonly("LastEditedBy", &BND_ONXModel::GetLastEditedBy)
-    //.def_property("Revision", &BND_ONXModel::GetRevision, &BND_ONXModel::SetRevision)
-    //.def_property_readonly("Settings", &BND_ONXModel::Settings)
-    //.def_property_readonly("Objects", &BND_ONXModel::Objects)
-    //.def_property_readonly("Materials", &BND_ONXModel::Materials)
-    //.def_property_readonly("Linetypes", &BND_ONXModel::Linetypes)
-    //.def_property_readonly("Bitmaps", &BND_ONXModel::Bitmaps)
-    .def_property_readonly("Layers", &BND_ONXModel::Layers)
-    //.def_property_readonly("Groups", &BND_ONXModel::AllGroups)
-    //.def_property_readonly("DimStyles", &BND_ONXModel::DimStyles)
-    //.def_property_readonly("InstanceDefinitions", &BND_ONXModel::InstanceDefinitions)
-    //.def_property_readonly("Views", &BND_ONXModel::Views)
-    //.def_property_readonly("NamedViews", &BND_ONXModel::NamedViews)
-    //.def_property_readonly("PlugInData", &BND_ONXModel::PlugInData)
-    //.def_property_readonly("Strings", &BND_ONXModel::Strings)
-    //.def_property_readonly("EmbeddedFiles", &BND_ONXModel::EmbeddedFiles)
-    //.def_property_readonly("RenderContent", &BND_ONXModel::RenderContent)
-    //.def("Encode", &BND_ONXModel::Encode)
-    //.def("Encode", &BND_ONXModel::Encode2)
-    //.def("Decode", &BND_ONXModel::Decode)
-    //.def("EmbeddedFilePaths", &BND_ONXModel::GetEmbeddedFilePaths)
-    //.def("EmbeddedFilePaths2", &BND_ONXModel::GetEmbeddedFilePaths2)
-    //.def("GetEmbeddedFileAsBase64", &BND_ONXModel::GetEmbeddedFileAsBase64)
-    //.def("GetEmbeddedFileAsBase64", &BND_ONXModel::GetEmbeddedFileAsBase64Strict)
-    //.def("RdkXml", &BND_ONXModel::RdkXml)
+    .def("write", &BND_ONXModel::Write, py::arg("path"), py::arg("version")=7)
+    .def_property_readonly("layers", &BND_ONXModel::Layers)
     ;
 }
--- src/model.h
+++ src/model.h
@@ -6,11 +6,30 @@ void ModelBindings(rh3dmpymodule& m);
 
 class BND_ONXModel {
 public:
-    std::shared_ptr<ONX_Model> m_model;
+    ///@name Public member variables
+    ///@{
+    std::shared_ptr<ONX_Model> p_model;
 
+    ///@}
+    ///@name Life Cycle
+    ///@{
+
+    /**
+    * Default constructor
+    */
     BND_ONXModel();
+
+    /**
+    * Constructor using Opennurbs model
+    */
     BND_ONXModel(ONX_Model* m);
+
+    /**
+    * Destructor
+    */
     void Destroy();
+    ///@}
+
     static BND_ONXModel* Read(std::wstring path);
     static std::string ReadNotes(std::wstring path);
     static int ReadArchiveVersion(std::wstring path);
@@ -31,24 +50,6 @@ public:
     BND_DateTime GetCreated() const;
     BND_DateTime GetLastEdited() const;
     int GetRevision() const;
-    void SetRevision(int r);
-    //BND_File3dmSettings Settings() { return BND_File3dmSettings(m_model); }
-
-    //BND_ONXModel_ObjectTable Objects() { return BND_ONXModel_ObjectTable(m_model); }
-    //BND_File3dmMaterialTable Materials() { return BND_File3dmMaterialTable(m_model); }
-    //BND_File3dmLinetypeTable Linetypes() { return BND_File3dmLinetypeTable(m_model); }
-    //BND_File3dmBitmapTable Bitmaps() { return BND_File3dmBitmapTable(m_model); }
-    BND_File3dmLayerTable Layers() { return BND_File3dmLayerTable(m_model); }
-    //BND_File3dmGroupTable AllGroups() { return BND_File3dmGroupTable(m_model); }
-    //BND_File3dmDimStyleTable DimStyles() { return BND_File3dmDimStyleTable(m_model); }
-    //BND_File3dmInstanceDefinitionTable InstanceDefinitions() { return BND_File3dmInstanceDefinitionTable(m_model); }
-    //BND_File3dmViewTable Views() { return BND_File3dmViewTable(m_model, false); }
-    //BND_File3dmViewTable NamedViews() { return BND_File3dmViewTable(m_model, true); }
-    //BND_File3dmPlugInDataTable PlugInData() { return BND_File3dmPlugInDataTable(m_model); }
-    //BND_File3dmStringTable Strings() { return BND_File3dmStringTable(m_model); }
-    //BND_File3dmEmbeddedFileTable EmbeddedFiles() { return BND_File3dmEmbeddedFileTable(m_model); }
-    //BND_File3dmRenderContentTable RenderContent() { return BND_File3dmRenderContentTable(m_model); }
-
-    BND_TUPLE GetEmbeddedFilePaths();
-    std::vector<std::wstring> GetEmbeddedFilePaths2();
+    void SetRevision(int revision_number);
+    BND_File3dmLayerTable Layers();
 };

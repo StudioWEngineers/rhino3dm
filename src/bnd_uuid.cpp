3894fb3d6a45fef731609cceaf36bbda4f4aeebd
StudioWEngineers <studio.w.engineers@gmail.com>
2025-04-30 14:39:11 +0200

    remove unused function from bnd_uuid.cpp

--- src/bnd_uuid.cpp
+++ src/bnd_uuid.cpp
@@ -25,14 +25,3 @@ ON_UUID Binding_to_ON_UUID(const BND_UUID& id)
 #endif
   return ON_UuidFromString(s.c_str());
 }
-
-std::vector<BND_UUID> ON_SimpleArrayUUID_to_Binding(const ON_SimpleArray<ON_UUID>& uuids)
-{
-	std::vector<BND_UUID> guids;
-	for (int i = 0; i < uuids.Count(); i++)
-	{
-		guids.push_back(ON_UUID_to_Binding(uuids[i]));
-	}
-
-	return guids;
-}

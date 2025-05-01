3881b94a431840f798b8dfe334fbdcfeb5072209
StudioWEngineers <studio.w.engineers@gmail.com>
2025-05-01 17:55:18 +0200

    layer: separation header and source, style

--- src/layer.cpp
+++ src/layer.cpp
@@ -1,167 +1,232 @@
 #include "bindings.h"
 
-Layer::Layer()
-{
-  SetTrackedPointer(new ON_Layer(), nullptr);
+Layer::Layer() {
+    SetTrackedPointer(new ON_Layer(), nullptr);
 }
 
-Layer::Layer(ON_Layer* layer, const ON_ModelComponentReference* compref)
-{
-  SetTrackedPointer(layer, compref);
+Layer::Layer(ON_Layer* layer, const ON_ModelComponentReference* compref) {
+    SetTrackedPointer(layer, compref);
 }
 
-Layer::Layer(ON_Layer* layer, const ON_ModelComponentReference* compref, std::shared_ptr<ONX_Model>& model)
-{
-  SetTrackedPointer(layer, compref);
-  m_model = model;
+Layer::Layer(ON_Layer* layer, const ON_ModelComponentReference* compref, std::shared_ptr<ONX_Model>& model) {
+    SetTrackedPointer(layer, compref);
+    m_model = model;
 }
 
 Layer::~Layer() {
-  if (m_layer != nullptr && m_component_ref.IsEmpty()) {
-    delete m_layer;
-  }
+    if (m_layer != nullptr && m_component_ref.IsEmpty()) {
+        delete m_layer;
+        m_layer = nullptr;
+    }
 }
 
-void Layer::SetTrackedPointer(ON_Layer* layer, const ON_ModelComponentReference* compref)
-{
-  m_layer = layer;
-  if( compref )
-  {
-    m_component_ref = *compref;
-  }
-  else
-  {
-    ON_ModelComponent* model_component = ON_ModelComponent::Cast(layer);
-    if (nullptr == model_component)
-      model_component = ON_ModelGeometryComponent::CreateManaged(layer, nullptr, nullptr);
-    if (model_component)
-      m_component_ref = ON_ModelComponentReference::CreateForExperts(model_component, true);
-  }
-  //BND_CommonObject::SetTrackedPointer(layer, compref);
+void Layer::SetTrackedPointer(ON_Layer* layer, const ON_ModelComponentReference* compref) {
+    m_layer = layer;
+
+    if (compref) {
+        m_component_ref = *compref;
+    } else {
+        ON_ModelComponent* model_component = ON_ModelComponent::Cast(layer);
+        if (model_component == nullptr) {
+            model_component = ON_ModelGeometryComponent::CreateManaged(layer, nullptr, nullptr);
+        }
+        if (model_component) {
+            m_component_ref = ON_ModelComponentReference::CreateForExperts(model_component, true);
+        }
+    }
 }
 
-//void BND_CommonObject::SetTrackedPointer(ON_Object* obj, const ON_ModelComponentReference* compref)
-//{
-//  if( compref )
-//  {
-//    m_component_ref = *compref;
-//  }
-//  else
-//  {
-//    ON_ModelComponent* model_component = ON_ModelComponent::Cast(obj);
-//    if (nullptr == model_component)
-//      model_component = ON_ModelGeometryComponent::CreateManaged(obj, nullptr, nullptr);
-//    if (model_component)
-//      m_component_ref = ON_ModelComponentReference::CreateForExperts(model_component, true);
-//  }
-//  m_object = obj;
-//}
-
-std::wstring Layer::GetFullPath() const
-{
-  ONX_Model* model = m_model.get();
-  if (nullptr == model)
-    return GetName();
-
-  ON_wString fullPath = m_layer->Name();
-  ON_UUID parent_id = m_layer->ParentId();
-  while (ON_UuidIsNotNil(parent_id))
-  {
-    ON_ModelComponentReference compref = model->LayerFromId(parent_id);
-    const ON_ModelComponent* model_component = compref.ModelComponent();
-    ON_Layer* modellayer = const_cast<ON_Layer*>(ON_Layer::Cast(model_component));
-    if (nullptr == modellayer)
-      break;
-
-    ON_wString parentName = modellayer->Name();
-    fullPath = parentName + ON_ModelComponent::NamePathSeparator + fullPath;
-    parent_id = modellayer->ParentId();
-  }
-
-  return std::wstring(fullPath.Array());
+std::wstring Layer::PathSeparator() {
+    return std::wstring(ON_ModelComponent::NamePathSeparator.Array());
 }
 
-bool Layer::HasPerViewportSettings(BND_UUID viewportId) const
-{
-  ON_UUID id = Binding_to_ON_UUID(viewportId);
-  return m_layer->HasPerViewportSettings(id);
+std::wstring Layer::GetFullPath() const {
+    if (!m_model) {
+        return GetName();
+    }
+
+    ONX_Model* model = m_model.get();
+    ON_wString fullPath = m_layer->Name();
+    ON_UUID parent_id = m_layer->ParentId();
+
+    while (ON_UuidIsNotNil(parent_id)) {
+        ON_ModelComponentReference compref = model->LayerFromId(parent_id);
+        const ON_ModelComponent* model_component = compref.ModelComponent();
+        ON_Layer* parent_layer = const_cast<ON_Layer*>(ON_Layer::Cast(model_component));
+        if (!parent_layer) {
+            break;
+        }
+
+        ON_wString parentName = parent_layer->Name();
+        fullPath = parentName + ON_ModelComponent::NamePathSeparator + fullPath;
+        parent_id = parent_layer->ParentId();
+    }
+
+    return std::wstring(fullPath.Array());
 }
 
-void Layer::DeletePerViewportSettings(BND_UUID viewportId)
-{
-  ON_UUID id = Binding_to_ON_UUID(viewportId);
-  m_layer->DeletePerViewportSettings(id);
+std::wstring Layer::GetName() const {
+    return std::wstring(m_layer->NameAsPointer());
 }
 
-BND_Color Layer::GetColor() const
-{
-  ON_Color c = m_layer->Color();
-  return ON_Color_to_Binding(c);
-}
-void Layer::SetColor(const BND_Color& color)
-{
-  ON_Color c = Binding_to_ON_Color(color);
-  m_layer->SetColor(c);
+void Layer::SetName(const std::wstring& name) {
+    m_layer->SetName(name.c_str());
 }
 
-BND_Color Layer::PerViewportColor(BND_UUID viewportId) const
-{
-  ON_UUID id = Binding_to_ON_UUID(viewportId);
-  ON_Color c = m_layer->PerViewportColor(id);
-  return ON_Color_to_Binding(c);
-}
-void Layer::SetPerViewportColor(BND_UUID viewportId, BND_Color color)
-{
-  ON_UUID id = Binding_to_ON_UUID(viewportId);
-  ON_Color c = Binding_to_ON_Color(color);
-  m_layer->SetPerViewportColor(id, c);
-}
-void Layer::DeletePerViewportColor(BND_UUID viewportId)
-{
-  ON_UUID id = Binding_to_ON_UUID(viewportId);
-  m_layer->DeletePerViewportColor(id);
+bool Layer::HasPerViewportSettings(BND_UUID viewportId) const {
+    ON_UUID id = Binding_to_ON_UUID(viewportId);
+    return m_layer->HasPerViewportSettings(id);
 }
 
-BND_Color Layer::GetPlotColor() const
-{
-  ON_Color c = m_layer->PlotColor();
-  return ON_Color_to_Binding(c);
-}
-void Layer::SetPlotColor(const BND_Color& color)
-{
-  ON_Color c = Binding_to_ON_Color(color);
-  m_layer->SetPlotColor(c);
+void Layer::DeletePerViewportSettings(BND_UUID viewportId) {
+    ON_UUID id = Binding_to_ON_UUID(viewportId);
+    m_layer->DeletePerViewportSettings(id);
 }
 
-void LayerBindings(rh3dmpymodule& m)
-{
-  py::class_<Layer>(m, "Layer")
-    .def(py::init<>())
-    .def_property_readonly_static("PathSeparator", &Layer::PathSeparator)
-    .def_property("Name", &Layer::GetName, &Layer::SetName)
-    .def_property_readonly("FullPath", &Layer::GetFullPath)
-    .def_property("Id", &Layer::GetId, &Layer::SetId)
-    .def_property_readonly("Index", &Layer::GetIndex)
-    .def_property("ParentLayerId", &Layer::GetParentLayerId, &Layer::SetParentLayerId)
-    .def_property("IgesLevel", &Layer::GetIgesLevel, &Layer::SetIgesLevel)
-    .def("HasPerViewportSettings", &Layer::HasPerViewportSettings, py::arg("viewportId"))
-    .def("DeletePerViewportSettings", &Layer::DeletePerViewportSettings, py::arg("viewportId"))
-    .def_property("Color", &Layer::GetColor, &Layer::SetColor)
-    .def("PerViewportColor", &Layer::PerViewportColor, py::arg("viewportId"))
-    .def("SetPerViewportColor", &Layer::SetPerViewportColor, py::arg("viewportId"), py::arg("color"))
-    .def("DeletePerViewportColor", &Layer::DeletePerViewportColor, py::arg("viewportId"))
-    .def_property("PlotColor", &Layer::GetPlotColor, &Layer::SetPlotColor)
-    .def_property("PlotWeight", &Layer::GetPlotWeight, &Layer::SetPlotWeight)
-    .def_property("LinetypeIndex", &Layer::GetLinetypeIndex, &Layer::SetLinetypeIndex)
-    .def_property("RenderMaterialIndex", &Layer::GetRenderMaterialIndex, &Layer::SetRenderMaterialIndex)
-    .def_property("Visible", &Layer::IsVisible, &Layer::SetVisible)
-    .def_property("Locked", &Layer::IsLocked, &Layer::SetLocked)
-    .def("GetPersistentVisibility", &Layer::GetPersistentVisibility)
-    .def("SetPersistentVisibility", &Layer::SetPersistentVisibility, py::arg("persistentVisibility"))
-    .def("UnsetPersistentVisibility", &Layer::UnsetPersistentVisibility)
-    .def("GetPersistentLocking", &Layer::GetPersistentLocking)
-    .def("SetPersistentLocking", &Layer::SetPersistentLocking, py::arg("persistentLocking"))
-    .def("UnsetPersistentLocking", &Layer::UnsetPersistentLocking)
-    .def_property("Expanded", &Layer::IsExpanded, &Layer::SetExpanded)
-    ;
+BND_Color Layer::GetColor() const {
+    return ON_Color_to_Binding(m_layer->Color());
+}
+
+void Layer::SetColor(const BND_Color& color) {
+    m_layer->SetColor(Binding_to_ON_Color(color));
+}
+
+BND_UUID Layer::GetParentLayerId() const {
+    return ON_UUID_to_Binding(m_layer->ParentId());
+}
+
+void Layer::SetParentLayerId(BND_UUID id) {
+    m_layer->SetParentLayerId(Binding_to_ON_UUID(id));
+}
+
+int Layer::GetIgesLevel() const {
+    return m_layer->IgesLevel();
+}
+
+void Layer::SetIgesLevel(int level) {
+    m_layer->SetIgesLevel(level);
+}
+
+BND_Color Layer::PerViewportColor(BND_UUID viewportId) const {
+    ON_UUID id = Binding_to_ON_UUID(viewportId);
+    return ON_Color_to_Binding(m_layer->PerViewportColor(id));
+}
+
+void Layer::SetPerViewportColor(BND_UUID viewportId, BND_Color color) {
+    ON_UUID id = Binding_to_ON_UUID(viewportId);
+    m_layer->SetPerViewportColor(id, Binding_to_ON_Color(color));
+}
+
+void Layer::DeletePerViewportColor(BND_UUID viewportId) {
+    ON_UUID id = Binding_to_ON_UUID(viewportId);
+    m_layer->DeletePerViewportColor(id);
+}
+
+BND_Color Layer::GetPlotColor() const {
+    return ON_Color_to_Binding(m_layer->PlotColor());
+}
+
+bool Layer::IsExpanded() const {
+    return m_layer->m_bExpanded;
+}
+
+void Layer::SetExpanded(bool e) {
+    m_layer->m_bExpanded = e;
+}
+
+void Layer::SetPlotColor(const BND_Color& color) {
+    m_layer->SetPlotColor(Binding_to_ON_Color(color));
+}
+
+double Layer::GetPlotWeight() const {
+    return m_layer->PlotWeight();
+}
+
+void Layer::SetPlotWeight(double weight) {
+    m_layer->SetPlotWeight(weight);
+}
+
+int Layer::GetLinetypeIndex() const {
+    return m_layer->LinetypeIndex();
+}
+
+void Layer::SetLinetypeIndex(int index) {
+    m_layer->SetLinetypeIndex(index);
+}
+
+int Layer::GetRenderMaterialIndex() const {
+    return m_layer->RenderMaterialIndex();
+}
+
+void Layer::SetRenderMaterialIndex(int index) {
+    m_layer->SetRenderMaterialIndex(index);
+}
+
+bool Layer::IsVisible() const {
+    return m_layer->IsVisible();
+}
+
+void Layer::SetVisible(bool visible) {
+    m_layer->SetVisible(visible);
+}
+
+bool Layer::IsLocked() const {
+    return m_layer->IsLocked();
+}
+
+void Layer::SetLocked(bool locked) {
+    m_layer->SetLocked(locked);
+}
+
+bool Layer::GetPersistentVisibility() const {
+    return m_layer->PersistentVisibility();
+}
+
+void Layer::SetPersistentVisibility(bool persistent) {
+    m_layer->SetPersistentVisibility(persistent);
+}
+
+void Layer::UnsetPersistentVisibility() {
+    m_layer->UnsetPersistentVisibility();
+}
+
+bool Layer::GetPersistentLocking() const {
+    return m_layer->PersistentLocking();
+}
+
+void Layer::SetPersistentLocking(bool persistent) {
+    m_layer->SetPersistentLocking(persistent);
+}
+
+void Layer::UnsetPersistentLocking() {
+    m_layer->UnsetPersistentLocking();
+}
+
+void LayerBindings(rh3dmpymodule& m) {
+    py::class_<Layer>(m, "Layer")
+        .def(py::init<>())
+        .def_property_readonly_static("PathSeparator", &Layer::PathSeparator)
+        .def_property("Name", &Layer::GetName, &Layer::SetName)
+        .def_property_readonly("FullPath", &Layer::GetFullPath)
+        .def_property("ParentLayerId", &Layer::GetParentLayerId, &Layer::SetParentLayerId)
+        .def_property("IgesLevel", &Layer::GetIgesLevel, &Layer::SetIgesLevel)
+        .def("HasPerViewportSettings", &Layer::HasPerViewportSettings, py::arg("viewportId"))
+        .def("DeletePerViewportSettings", &Layer::DeletePerViewportSettings, py::arg("viewportId"))
+        .def_property("Color", &Layer::GetColor, &Layer::SetColor)
+        .def("PerViewportColor", &Layer::PerViewportColor, py::arg("viewportId"))
+        .def("SetPerViewportColor", &Layer::SetPerViewportColor, py::arg("viewportId"), py::arg("color"))
+        .def("DeletePerViewportColor", &Layer::DeletePerViewportColor, py::arg("viewportId"))
+        .def_property("PlotColor", &Layer::GetPlotColor, &Layer::SetPlotColor)
+        .def_property("PlotWeight", &Layer::GetPlotWeight, &Layer::SetPlotWeight)
+        .def_property("LinetypeIndex", &Layer::GetLinetypeIndex, &Layer::SetLinetypeIndex)
+        .def_property("RenderMaterialIndex", &Layer::GetRenderMaterialIndex, &Layer::SetRenderMaterialIndex)
+        .def_property("Visible", &Layer::IsVisible, &Layer::SetVisible)
+        .def_property("Locked", &Layer::IsLocked, &Layer::SetLocked)
+        .def("GetPersistentVisibility", &Layer::GetPersistentVisibility)
+        .def("SetPersistentVisibility", &Layer::SetPersistentVisibility, py::arg("persistentVisibility"))
+        .def("UnsetPersistentVisibility", &Layer::UnsetPersistentVisibility)
+        .def("GetPersistentLocking", &Layer::GetPersistentLocking)
+        .def("SetPersistentLocking", &Layer::SetPersistentLocking, py::arg("persistentLocking"))
+        .def("UnsetPersistentLocking", &Layer::UnsetPersistentLocking)
+        .def_property("Expanded", &Layer::IsExpanded, &Layer::SetExpanded);
 }
--- src/layer.h
+++ src/layer.h
@@ -1,90 +1,73 @@
-#include "bindings.h"
-
 #pragma once
+
+#include "bindings.h"
 
 void LayerBindings(rh3dmpymodule& m);
 
-class Layer {  //: public BND_CommonObject
-    public:
+class Layer {
+public:
     ON_Layer* m_layer = nullptr;
-    std::shared_ptr<ONX_Model> m_model;
 
     Layer();
-
-    Layer(
-      ON_Layer* layer,
-      const ON_ModelComponentReference* compref
-    );
-
-    Layer(
-      ON_Layer* layer,
-      const ON_ModelComponentReference* compref,
-      std::shared_ptr<ONX_Model>& model
-    );
-
+    Layer(ON_Layer* layer, const ON_ModelComponentReference* compref);
+    Layer(ON_Layer* layer, const ON_ModelComponentReference* compref, std::shared_ptr<ONX_Model>& model);
     ~Layer();
 
-  static std::wstring PathSeparator() { return std::wstring(ON_ModelComponent::NamePathSeparator.Array()); }
+    static std::wstring PathSeparator();
 
-  std::wstring GetName() const { return std::wstring(m_layer->NameAsPointer()); }
-  void SetName(const std::wstring& name) { m_layer->SetName(name.c_str()); }
-  std::wstring GetFullPath() const;
-  BND_UUID GetId() const { return ON_UUID_to_Binding(m_layer->Id()); }
-  void SetId(BND_UUID id) { m_layer->SetId(Binding_to_ON_UUID(id)); }
+    std::wstring GetName() const;
+    void SetName(const std::wstring& name);
 
-  int GetIndex() const { return m_layer->Index(); }
-  //public Guid ParentLayerId {get;set;}
-  BND_UUID GetParentLayerId() const { return ON_UUID_to_Binding(m_layer->ParentId()); }
-  void SetParentLayerId(BND_UUID id) { m_layer->SetParentLayerId(Binding_to_ON_UUID(id)); }
-  int GetIgesLevel() const { return m_layer->IgesLevel(); }
-  void SetIgesLevel(int level) { m_layer->SetIgesLevel(level); }
-  bool HasPerViewportSettings(BND_UUID viewportId) const;
-  void DeletePerViewportSettings(BND_UUID viewportId);
-  BND_Color GetColor() const;
-  void SetColor(const BND_Color& color);
-  BND_Color PerViewportColor(BND_UUID viewportId) const;
-  void SetPerViewportColor(BND_UUID viewportId, BND_Color color);
-  void DeletePerViewportColor(BND_UUID viewportId);
-  BND_Color GetPlotColor() const;
-  void SetPlotColor(const BND_Color& color);
-//  BND_Color PerViewportPlotColor(BND_UUID viewportId) const;
-//  void SetPerViewportPlotColor(BND_UUID viewportId, BND_Color color);
-//  void DeletePerViewportPlotColor(BND_UUID viewportId);
-  double GetPlotWeight() const { return m_layer->PlotWeight(); }
-  void SetPlotWeight(double weight) { m_layer->SetPlotWeight(weight); }
-//  double PerViewportPlotWeight(BND_UUID viewportId) const;
-//  void SetPerViewportPlotWeight(BND_UUID viewportId, double plotWeight);
-//  void DeletePerViewportPlotWeight(BND_UUID viewportId);
-  int GetLinetypeIndex() const { return m_layer->LinetypeIndex(); }
-  void SetLinetypeIndex(int index) { m_layer->SetLinetypeIndex(index); }
-  int GetRenderMaterialIndex() const { return m_layer->RenderMaterialIndex(); }
-  void SetRenderMaterialIndex(int index) { m_layer->SetRenderMaterialIndex(index); }
-  bool IsVisible() const { return m_layer->IsVisible(); }
-  void SetVisible(bool b) { m_layer->SetVisible(b); }
-//  bool PerViewportIsVisible(BND_UUID viewportId) const;
-//  void SetPerViewportVisible(BND_UUID viewportId, bool visible);
-//  void DeletePerViewportVisible(BND_UUID viewportId);
-//  bool PerViewportPersistentVisibility(BND_UUID viewportId) const;
-//  void SetPerViewportPersistentVisibility(BND_UUID viewportId, bool persistentVisibility);
-//  void UnsetPerViewportPersistentVisibility(BND_UUID viewportId);
-  bool IsLocked() const { return m_layer->IsLocked(); }
-  void SetLocked(bool l) { m_layer->SetLocked(l); }
-  bool GetPersistentVisibility() const { return m_layer->PersistentVisibility(); }
-  void SetPersistentVisibility(bool persistentVisibility) { m_layer->SetPersistentVisibility(persistentVisibility); }
-  void UnsetPersistentVisibility() { m_layer->UnsetPersistentVisibility(); }
-  bool GetPersistentLocking() const { return m_layer->PersistentLocking(); }
-  void SetPersistentLocking(bool persistentLocking) { m_layer->SetPersistentLocking(persistentLocking); }
-  void UnsetPersistentLocking() { m_layer->UnsetPersistentLocking(); }
-  bool IsExpanded() const { return m_layer->m_bExpanded; }
-  void SetExpanded(bool e) { m_layer->m_bExpanded = e; }
-  //public void CopyAttributesFrom(Layer otherLayer)
-  //public bool SetUserString(string key, string value)
-  //public string GetUserString(string key)
-  //public int UserStringCount {get;}
-  //public System.Collections.Specialized.NameValueCollection GetUserStrings()
+    std::wstring GetFullPath() const;
 
+    BND_UUID GetParentLayerId() const;
+    void SetParentLayerId(BND_UUID id);
+
+    int GetIgesLevel() const;
+    void SetIgesLevel(int level);
+
+    bool HasPerViewportSettings(BND_UUID viewportId) const;
+    void DeletePerViewportSettings(BND_UUID viewportId);
+
+    BND_Color GetColor() const;
+    void SetColor(const BND_Color& color);
+
+    BND_Color PerViewportColor(BND_UUID viewportId) const;
+    void SetPerViewportColor(BND_UUID viewportId, BND_Color color);
+    void DeletePerViewportColor(BND_UUID viewportId);
+
+    BND_Color GetPlotColor() const;
+    void SetPlotColor(const BND_Color& color);
+
+    double GetPlotWeight() const;
+    void SetPlotWeight(double weight);
+
+    int GetLinetypeIndex() const;
+    void SetLinetypeIndex(int index);
+
+    int GetRenderMaterialIndex() const;
+    void SetRenderMaterialIndex(int index);
+
+    bool IsVisible() const;
+    void SetVisible(bool visible);
+
+    bool IsLocked() const;
+    void SetLocked(bool locked);
+
+    bool GetPersistentVisibility() const;
+    void SetPersistentVisibility(bool persistent);
+    void UnsetPersistentVisibility();
+
+    bool GetPersistentLocking() const;
+    void SetPersistentLocking(bool persistent);
+    void UnsetPersistentLocking();
+
+    bool IsExpanded() const;
+    void SetExpanded(bool expanded);
 
 protected:
-    ON_ModelComponentReference m_component_ref; // holds shared pointer for this class
     void SetTrackedPointer(ON_Layer* layer, const ON_ModelComponentReference* compref);
+
+    std::shared_ptr<ONX_Model> m_model;
+    ON_ModelComponentReference m_component_ref;
 };

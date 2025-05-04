dcad6abe95c90166da8a215df134b80a89fbe9c5
StudioWEngineers <studio.w.engineers@gmail.com>
2025-05-04 12:24:43 +0200

    remove bnd_uuid

--- src/bindings.h
+++ src/bindings.h
@@ -110,7 +110,7 @@ typedef py::list BND_LIST;
 //BND_DateTime CreateDateTime(struct tm t);
 
 //#include "bnd_color.h"
-#include "bnd_uuid.h"
+//#include "bnd_uuid.h"
 //#include "layer_view.h"
 //#include "bnd_layer_table.h"
 #include "layer.h"
--- src/bnd_uuid.h
+++
@@ -1,8 +0,0 @@
-#include "bindings.h"
-
-#pragma once
-
-typedef py::object BND_UUID;
-
-BND_UUID ON_UUID_to_Binding(const ON_UUID& id);
-ON_UUID Binding_to_ON_UUID(const BND_UUID& id);

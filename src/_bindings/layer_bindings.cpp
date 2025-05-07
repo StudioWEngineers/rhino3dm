c416ae283a76fd25fddf0906cf8b9a3f27355771
StudioWEngineers <studio.w.engineers@gmail.com>
2025-05-07 08:07:41 +0200

    updating layer_bindings.cpp

--- src/_bindings/layer_bindings.cpp
+++ src/_bindings/layer_bindings.cpp
@@ -1,26 +1,30 @@
-#include "layer_bindings.h"
 #include "../layer.h"
-#include "uuid_caster.h"
 #include "color_caster.h"
+#include "layer_bindings.h"
+#include "uuid_caster.h"
 
-namespace py = nanobind;
+namespace nb = nanobind;
 
 void LayerBindings(nanobind::module_ &m) {
-    py::class_<Layer>(m, "Layer")
+    nb::class_<Layer>(m, "Layer")
 
-        .def(py::init<>())
+        .def(nb::init<>())
         .def("__repr__", &Layer::ToString)
 
-        .def("get_name", &Layer::GetName, "Returns the Layer's name.")
-        .def("get_plot_color", &Layer::GetPlotColor, "Returns the Layer's plot color.")
+        .def_prop_ro("full_path", &Layer::GetFullPath)
 
-        .def("set_name", &Layer::SetName, "Sets the Layer's name.", py::arg("name"))
-        .def("set_plot_color", &Layer::SetPlotColor, "Sets the Layer's plot color.", py::arg("new_color"))
+        .def_prop_ro_static("path_separator", &Layer::PathSeparator)
 
-        .def("get_full_path", &Layer::GetFullPath, "Returns the full path to this Layer, including nesting information.")
-
+        .def_prop_rw("color", &Layer::GetColor, &Layer::SetColor)
         .def_prop_rw("iges_level", &Layer::GetIgesLevel, &Layer::SetIgesLevel)
-
-        .def("get_parent_layer_id", &Layer::GetParentLayerId)
+        .def_prop_rw("is_expanded", &Layer::IsExpanded, &Layer::SetExpanded)
+        .def_prop_rw("is_locked", &Layer::IsLocked, &Layer::SetLocked)
+        .def_prop_rw("is_visible", &Layer::IsVisible, &Layer::SetVisible)
+        .def_prop_rw("line_type_index", &Layer::GetLinetypeIndex, &Layer::SetLinetypeIndex)
+        .def_prop_rw("name", &Layer::GetName, &Layer::SetName)
+        .def_prop_rw("parent_uuid", &Layer::GetParentLayerId, &Layer::SetParentLayerId)
+        .def_prop_rw("plot_color", &Layer::GetPlotColor, &Layer::SetPlotColor)
+        .def_prop_rw("plot_weight", &Layer::GetPlotWeight, &Layer::SetPlotWeight)
+        .def_prop_rw("render_material_index", &Layer::GetRenderMaterialIndex, &Layer::SetRenderMaterialIndex)
     ;
 }

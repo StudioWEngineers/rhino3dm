f63c77c2f4cef83343f5fd443e1286f138f64cff
StudioWEngineers <studio.w.engineers@gmail.com>
2025-05-09 12:00:22 +0200

    working on layer_view...

--- src/_bindings/layer_view_bindings.cpp
+++ src/_bindings/layer_view_bindings.cpp
@@ -12,9 +12,10 @@ void LayerViewBindings(nb::module_ &m) {
         .def_prop_ro("color", &LayerView::GetColor)
         .def_prop_ro("full_path", &LayerView::GetFullPath)
         .def_prop_ro("iges_level", &LayerView::GetIgesLevel)
-        .def_prop_ro("is_expanded", &LayerView::IsExpanded)
-        .def_prop_ro("is_locked", &LayerView::IsLocked)
-        .def_prop_ro("is_visible", &LayerView::IsVisible)
+        .def_prop_ro("is_expanded", &LayerView::GetIsExpanded)
+        .def_prop_ro("is_locked", &LayerView::GetIsLocked)
+        .def_prop_ro("is_visible", &LayerView::GetIsVisible)
+        .def_prop_ro("layer_uuid", &LayerView::GetLayerId)
         .def_prop_ro("line_type_index", &LayerView::GetLinetypeIndex)
         .def_prop_ro("name", &LayerView::GetName)
         .def_prop_ro("parent_uuid", &LayerView::GetParentLayerId)
@@ -23,8 +24,7 @@ void LayerViewBindings(nb::module_ &m) {
         .def_prop_ro("plot_color", &LayerView::GetPlotColor)
         .def_prop_ro("plot_weight", &LayerView::GetPlotWeight)
         .def_prop_ro("render_material_index", &LayerView::GetRenderMaterialIndex)
-        .def_prop_ro("layer_uuid", &LayerView::GetId)
 
-        .def_prop_ro_static("path_separator", &LayerView::PathSeparator)
+        .def_prop_ro_static("path_separator", &LayerView::GetPathSeparator)
     ;
 }
--- src/layer_view.h
+++ src/layer_view.h
@@ -1,53 +1,49 @@
+/*
+    src/layer_view.h: Implementation details related to LayerView, a ON_Layer already
+    added to the model.
+    Rearranged from .../bnd_layer.h in https://github.com/mcneel/rhino3dm
+
+    Copyright (c) 2025 Studio W Engineers
+
+    All rights reserved. Use of this source code is governed by a
+    BSD-style license that can be found in the LICENSE file.
+*/
+
 #pragma once
 
 #include "opennurbs_includes.h"
 
 class LayerView {
 public:
-    LayerView(ON_Layer* layer, const ON_ModelComponentReference* compref);
-    LayerView(ON_Layer* layer, const ON_ModelComponentReference* compref, std::shared_ptr<ONX_Model>& model);
+    LayerView(ON_Layer* layer, const ON_ModelComponentReference* comp_ref);
+    LayerView(ON_Layer* layer, const ON_ModelComponentReference* comp_ref, std::shared_ptr<ONX_Model>& model);
     ~LayerView();
 
     const ON_Layer* LayerHandle() const;
 
-    static std::wstring PathSeparator();
-
-    ON_UUID GetId() const;
-
-    std::wstring GetName() const;
-
-    std::wstring GetFullPath() const;
-
-    ON_UUID GetParentLayerId() const;
-
-    int GetIgesLevel() const;
-
     ON_Color GetColor() const;
-
-    ON_Color GetPlotColor() const;
-
-    double GetPlotWeight() const;
-
+    std::wstring GetFullPath() const;
+    int GetIgesLevel() const;
+    bool GetIsExpanded() const;
+    bool GetIsLocked() const;
+    bool GetIsVisible() const;
+    ON_UUID GetLayerId() const;
     int GetLinetypeIndex() const;
-
-    int GetRenderMaterialIndex() const;
-
-    bool IsVisible() const;
-
-    bool IsLocked() const;
-
-    bool GetPersistentVisibility() const;
-
+    std::wstring GetName() const;
+    ON_UUID GetParentLayerId() const;
+    static std::wstring GetPathSeparator();
     bool GetPersistentLocking() const;
-
-    bool IsExpanded() const;
+    bool GetPersistentVisibility() const;
+    ON_Color GetPlotColor() const;
+    double GetPlotWeight() const;
+    int GetRenderMaterialIndex() const;
 
     const std::string ToString() const;
 
 private:
+    ON_ModelComponentReference m_comp_ref;
     ON_Layer* m_layer = nullptr;
     std::shared_ptr<ONX_Model> m_model;
-    ON_ModelComponentReference m_comp_ref;
 
-    void SetTrackedPointer(ON_Layer* layer, const ON_ModelComponentReference* compref);
+    void SetTrackedPointer(ON_Layer* layer, const ON_ModelComponentReference* comp_ref);
 };

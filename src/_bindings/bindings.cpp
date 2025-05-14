9ab5ceb3f5a09cbf824f9cbc5e06cc1359625c0d
StudioWEngineers <studio.w.engineers@gmail.com>
2025-05-14 16:35:38 +0200

    minor in binding docs

--- src/_bindings/bindings.cpp
+++ src/_bindings/bindings.cpp
@@ -5,7 +5,7 @@
 #include "layer_table_bindings.h"
 
 NB_MODULE(_rhino3dm, m) {
-    m.doc() = "rhino3dm python package. OpenNURBS wrappers with a RhinoCommon style";
+    m.doc() = "OpenNURBS wrapper.";
 
     ON::Begin();
     LayerBindings(m);

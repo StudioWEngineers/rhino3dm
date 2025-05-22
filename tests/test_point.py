6761404e288104a1396f021d32b898b01149adbb
StudioWEngineers <studio.w.engineers@gmail.com>
2025-05-22 08:59:35 +0200

    more on Point

--- tests/test_point.py
+++ tests/test_point.py
@@ -16,6 +16,7 @@ __email__ = "studio.w.engineers@gmail.com"
 __status__ "Release"
 """
 # standard library imports
+from math import sqrt
 from unittest import TestCase
 
 # third party library imports
@@ -27,6 +28,12 @@ from rhino3dm import Point
 class PointTestSuite(TestCase):
     """Tests for the `Point` class.
     """
+    def test_distance_to(self) -> None:
+        """Tests for the `distance_to` method.
+        """
+        point = Point(0, 0, 0)
+        self.assertEqual(point.distance_to(Point(1, 1, 1)), sqrt(3))
+
     def test_equal(self) -> None:
         """Tests for the `==` operator.
         """
@@ -81,6 +88,26 @@ class PointTestSuite(TestCase):
 
         with self.subTest(msg="Get z-coordinate after set"):
             self.assertEqual(point.z, 3.1)
+
+    def test_is_coincident(self) -> None:
+        """Tests for the `is_coincident` method.
+        """
+        point = Point(1, 2, 3.1)
+        self.assertTrue(point.is_coincident(Point(1, 2, 3.1)))
+
+    def test_multiplication(self) -> None:
+        """Tests for the `*` operator.
+        """
+        point = Point(0, 0, 1.1) * 2.1
+
+        with self.subTest(msg="x-coordinate"):
+            self.assertEqual(point.x, 0)
+
+        with self.subTest(msg="y-coordinate"):
+            self.assertEqual(point.y, 0)
+
+        with self.subTest(msg="z-coordinate"):
+            self.assertEqual(point.z, 1.1 * 2.1)
 
     def test_sum(self) -> None:
         """Tests for the `+` operator.

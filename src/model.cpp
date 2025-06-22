d96aef46cd7396d2e7c312580211bc396c8a019e
StudioWEngineers <studio.w.engineers@gmail.com>
2025-06-22 17:23:33 +0200

    alphabetical order in model.cpp

--- src/model.cpp
+++ src/model.cpp
@@ -3,9 +3,6 @@
 
 Model::Model() : p_model(std::make_shared<ONX_Model>()) {}
 
-void Model::Reset() {
-    p_model.reset();
-}
 
 bool Model::Read(std::wstring path) {
     p_model.reset(new ONX_Model());
@@ -18,8 +15,20 @@ bool Model::Read(std::wstring path) {
     return false;
 }
 
+void Model::Reset() {
+    p_model.reset();
+}
+
 bool Model::Write(std::wstring path, int version) {
     return p_model->Write(path.c_str(), version);
+}
+
+
+std::wstring Model::GetApplicationDetails() const {
+    if (p_model.get() != nullptr) {
+        return std::wstring(p_model->m_properties.m_Application.m_application_details);
+    }
+    return std::wstring();
 }
 
 std::wstring Model::GetApplicationName() const {
@@ -29,36 +38,11 @@ std::wstring Model::GetApplicationName() const {
     return std::wstring();
 }
 
-void Model::SetApplicationName(std::wstring comments) {
-    if (p_model.get() != nullptr) {
-        p_model->m_properties.m_Application.m_application_name = comments.c_str();
-    }
-}
-
 std::wstring Model::GetApplicationUrl() const {
     if (p_model.get() != nullptr) {
         return std::wstring(p_model->m_properties.m_Application.m_application_URL);
     }
     return std::wstring();
-}
-
-void Model::SetApplicationUrl(std::wstring s) {
-    if (p_model.get() != nullptr) {
-        p_model->m_properties.m_Application.m_application_URL = s.c_str();
-    }
-}
-
-std::wstring Model::GetApplicationDetails() const {
-    if (p_model.get() != nullptr) {
-        return std::wstring(p_model->m_properties.m_Application.m_application_details);
-    }
-    return std::wstring();
-}
-
-void Model::SetApplicationDetails(std::wstring s) {
-    if (p_model.get() != nullptr) {
-        p_model->m_properties.m_Application.m_application_details = s.c_str();
-    }
 }
 
 int Model::GetArchiveVersion() const {
@@ -87,11 +71,31 @@ int Model::GetRevision() const {
     return revision_number;
 }
 
+
+void Model::SetApplicationDetails(std::wstring s) {
+    if (p_model.get() != nullptr) {
+        p_model->m_properties.m_Application.m_application_details = s.c_str();
+    }
+}
+
+void Model::SetApplicationName(std::wstring comments) {
+    if (p_model.get() != nullptr) {
+        p_model->m_properties.m_Application.m_application_name = comments.c_str();
+    }
+}
+
+void Model::SetApplicationUrl(std::wstring s) {
+    if (p_model.get() != nullptr) {
+        p_model->m_properties.m_Application.m_application_URL = s.c_str();
+    }
+}
+
 void Model::SetRevision(int revision_number) {
     if (p_model.get() != nullptr) {
         p_model->m_properties.m_RevisionHistory.m_revision_count = revision_number;
     }
 }
+
 
 LayerTable Model::ModelLayerTable() {
     return LayerTable(p_model);
